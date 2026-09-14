9c885bc6d20fd9742450cc88c12810edee102f36
Nitin0069 <nitin4rana@gmail.com>
2026-09-14 16:22:05 +0530

    very imp

--- tailwind props/src/App.jsx
+++ tailwind props/src/App.jsx
@@ -1,15 +1,22 @@
 import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
 import './App.css'
-
+import Card from './components/Card'
+import Try from  './components/Try'
 function App() {
   const [count, setCount] = useState(0)
-
+let myOBj = {
+  name: "nitin",
+  age: 21
+}
   return (
     <>
-     <h2 className = 'bg-green-400 '> tailwind test </h2>
+
+      <h1 className="bg-green-400 text-black p-4 rounded-xl mb-4">Tailwind test</h1>
+
+      <Card channel = "1,2,3" maal = "leleleele" happ = "bhosdike" />
+      <Card channel = "2,3,4" maal = "nunnnu" happ = "hihiihi" />
+   <Try name = "Nitin" age = "34" />
+   <Try name = "AMan" />
     </>
   )
 }
---
+++ tailwind props/src/components/Try.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+function Try(props) {
+
+    return (
+        <>
+     <h2>Hello {props.name} , {props.age}</h2>
+     <h2>Hello {props.name}</h2>
+
+        </>
+    )
+}
+
+
+export default Try
